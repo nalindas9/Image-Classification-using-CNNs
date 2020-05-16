6d336ca81cdb1c7f4016bfc5be43f8bb36edfbb1
nalindas9 <nalindas9@gmail.com>
2020-05-16 12:11:01 -0400

    Add GPU implementation of VGG-16 CNN

--- Code/train.py
+++ Code/train.py
@@ -8,6 +8,7 @@ University of Maryland, College Park
 """
 import tensorflow as tf
 print("Tensorflow version:", tf.__version__)
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow import keras
@@ -15,6 +16,9 @@ import os
 import keras_preprocessing
 from keras_preprocessing import image
 from keras_preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from tensorflow.keras import optimizers
 
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
@@ -26,32 +30,57 @@ class myCallback(tf.keras.callbacks.Callback):
 def train(train_generator, validation_generator):
   callback = myCallback()
   try:
-    # Defining the CNN model here
-    model = keras.Sequential([
-      # First Convolution 
-      keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
-      keras.layers.MaxPooling2D(2,2),
-      # Second Convolution
-      keras.layers.Conv2D(64, (3,3), activation='relu'),
-      keras.layers.MaxPooling2D(2,2),
-      # Third Convolution 
-      keras.layers.Conv2D(128, (3,3), activation='relu'),
-      keras.layers.MaxPooling2D(2,2),
-      # Fourth Convolution
-      keras.layers.Conv2D(128, (3,3), activation='relu'),
-      keras.layers.MaxPooling2D(2,2),
-      # Flatten the convoluted data
-      keras.layers.Flatten(),
-      keras.layers.Dropout(0.5),
-      # Hidden dense layer
-      keras.layers.Dense(512, activation = 'relu'),
-      keras.layers.Dense(2, activation = 'softmax')
-    ])
+    # Defining the VGG-16 Architecture CNN model here
+    model = Sequential()
+ 
+    model.add(Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3),  padding="same"))
+    model.add(Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3),  padding="same"))
+    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(128, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(128, (3,3), activation='relu',  padding="same"))
+    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(256, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(256, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(256, (3,3), activation='relu',  padding="same"))
+    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(512, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(512, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(512, (3,3), activation='relu',  padding="same"))
+    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(512, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(512, (3,3), activation='relu',  padding="same"))
+    model.add(Conv2D(512, (3,3), activation='relu',  padding="same"))
+    model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(4096, activation = 'relu'))
+    model.add(Dense(4096, activation = 'relu'))
+    model.add(Dense(2, activation = 'softmax'))
     
-    model.compile(loss = 'categorical_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
+    lr_schedule = optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=1e-2,
+    decay_steps=10000,
+    decay_rate=0.9)
+
+    opt = optimizers.SGD(learning_rate=lr_schedule)
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy', 'categorical_crossentropy'], optimizer=opt)
     model.summary()
     
-    history = model.fit(train_generator, epochs=25, steps_per_epoch=20, validation_data = validation_generator, verbose = 1, callbacks=[callback])
+    history = model.fit(train_generator, epochs=50, steps_per_epoch=781, validation_data = validation_generator, verbose = 1, callbacks=[callback])
+    
+    acc = history.history['accuracy']
+    loss = history.history['categorical_crossentropy']
+    
+    epochs = range(len(acc))
+    
+    plt.plot(epochs, acc, 'g', label='Training accuracy')
+    plt.title('Training accuracy vs Epochs')
+    plt.legend(loc=0)
+    plt.show()
+    plt.plot(epochs, loss, 'r', label='Loss')
+    plt.title('Loss vs Epochs')
+    plt.legend(loc=0)
+    plt.show()
+    
   finally:
     model.save("trainedCNN_weights.h5")
     
@@ -72,7 +101,7 @@ def main():
 	  TRAINING_DIR,
 	  target_size=(150,150),
 	  class_mode='categorical',
-    batch_size=126
+ 	  batch_size=32
   )
 
   # Preparing testing dataset    
@@ -83,7 +112,7 @@ def main():
 	  VALIDATION_DIR,
 	  target_size=(150,150),
 	  class_mode='categorical',
-    batch_size=126
+	  batch_size=32  
   )
   
   train(train_generator, validation_generator)
